f00d64db4c862d6ccf618c6e9e68d7f4cdf0f0a4
Mipcra98 <willianfuhr27041998@gmail.com>
2024-11-01 00:40:28 -0300

    Actualización de RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function RegisterPage() {
-    const [name, setName] = useState('');
-    const [lastName, setLastName] = useState('');
+const RegisterPage = () => {
+    // const [form, setForm] = useState({})
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [contrasena, setContrasena] = useState('');
 
-
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const respuesta = await axios.post('/api/usuario', {
+            nombre,
+            apellido,
+            email,
+            contrasena
+        });
+        console.log(respuesta.data);
+    }
 
 
     return (
@@ -15,18 +26,18 @@ export default function RegisterPage() {
             <div className="mb-64">
 
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form  className="max-w-md mx-auto">
+                <form onSubmit={handleRegister} className="max-w-md mx-auto">
                     <input
                         type="text"
                         placeholder="Escriba su nombre..."
-                        value={name}
-                        onChange={e => setName(e.target.value)}
+                        value={nombre}
+                        onChange={e => setNombre(e.target.value)}
                     />
                     <input
                         type="text"
                         placeholder="Escriba su apellido..."
-                        value={lastName}
-                        onChange={e => setLastName(e.target.value)}
+                        value={apellido}
+                        onChange={e => setApellido(e.target.value)}
                     />
                     <input
                         type="email"
@@ -37,15 +48,17 @@ export default function RegisterPage() {
                     <input
                         type="password"
                         placeholder="Escriba su contraseña..."
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        value={contrasena}
+                        onChange={e => setContrasena(e.target.value)}
                     />
-                    <button className="primary">Register</button>
-                    <div className="text-center text-gray-500 py-2">
-                        Ya eres miembro? <Link className="underline text-black" to={'/login'}>Iniciar sesión</Link>
-                    </div>
+                    <button type="Submit" className="primary" >Register</button>
                 </form>
+                <div className="text-center text-gray-500 py-2">
+                    Ya eres miembro? <Link className="underline text-black" to={'/login'}>Iniciar sesión</Link>
+                </div>
             </div>
         </div>
     )
 }
+
+export default RegisterPage;
