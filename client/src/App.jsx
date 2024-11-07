17110f767ab6c9d43c5ca6007611e8b8039ce501
AalbanK <alban.m.krug@gmail.com>
2024-11-07 08:05:44 -0300

    update formulario para alojamientos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './Layout'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import PlacesPage from './pages/PlacesPage'
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route path='/index' index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/register' element={<RegisterPage />} />        
+        <Route path='/places' element={<PlacesPage />} />        
       </Route>
     </Routes>
 
