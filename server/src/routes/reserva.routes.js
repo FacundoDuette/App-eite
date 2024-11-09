2852a0fe16471fff84983a8dbca4ff15a3053619
Mipcra98 <willianfuhr27041998@gmail.com>
2024-11-09 16:47:39 -0300

    Willian - Desabilitando autenticación para acceso a datos del servidor

--- server/src/routes/reserva.routes.js
+++ server/src/routes/reserva.routes.js
@@ -5,24 +5,24 @@ import authenticate from '../../config/jwt.config.js';
 const router = express.Router();
 
 //Crear reserva
-router.post('/', authenticate, reservaController.createReserva);
+router.post('/'/* , authenticate */, reservaController.createReserva);
 
 //Obtener todas las reservas
-router.get('/', authenticate, reservaController.getAllReservas);
+router.get('/',/* authenticate, */ reservaController.getAllReservas);
 
 //Obtener reserva por id de reserva
-router.get('/:id', authenticate, reservaController.getReservaById);
+router.get('/:id',/* authenticate, */ reservaController.getReservaById);
 
 //Obtener reservas por id de usuario
-router.get('/user/:id', authenticate, reservaController.getReservaByusuario);
+router.get('/user/:id',/* authenticate, */ reservaController.getReservaByusuario);
 
 //Obtener reservas por id de alojamiento
-router.get('/host/:id', authenticate, reservaController.getReservaByalojamiento);
+router.get('/host/:id',/* authenticate, */ reservaController.getReservaByalojamiento);
 
 //Modificar reserva
-router.patch('/:id', authenticate, reservaController.updateReserva)
+router.patch('/:id',/* authenticate, */ reservaController.updateReserva)
 
 //Eliminar reserva
-router.delete('/:id', authenticate, reservaController.deleteReserva)
+router.delete('/:id',/* authenticate, */ reservaController.deleteReserva)
 
 export default router;
